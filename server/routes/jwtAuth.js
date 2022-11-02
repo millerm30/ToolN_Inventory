051c9f01bee2c866bddc6169be2aa24071fd4b97
Michael Miller <millergm30@gmail.com>
2022-11-02 19:19:29 -0230

    clean up...

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -8,10 +8,10 @@ const authorization = require('../middleware/authorization');
 //Register Route
 router.post('/register', validInfo, async (req, res) => {
   try {
-    // 1. Destructure req.body (name, email, password)
+    //Destructure req.body (name, email, password)
     const { name, email, password } = req.body;
 
-    // 2. Check if user doesn't exist (if user exists, throw error)
+    //Check if user doesn't exist (if user exists, throw error)
     const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [
       email
     ]);
@@ -19,21 +19,19 @@ router.post('/register', validInfo, async (req, res) => {
       return res.status(401).json('User already exists');
     }
 
-    // 3. Bcrypt the user's password
+    //Bcrypt the user's password
     const saltRound = 10;
     const salt = await bcrypt.genSalt(saltRound);
     const bcryptPassword = await bcrypt.hash(password, salt);
 
-    // 4. Enter the new user inside our database
+    //Enter the new user inside our database
     const newUser = await pool.query(
       'INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *', [name, email, bcryptPassword]
     );
 
-    // 5. Generating our jwt token
+    //Generating our jwt token
     const token = jwtGenerator(newUser.rows[0].user_id);
     res.json({ token });
-
-    // 6. Sending the token to the client
 
   } catch (err) {
     console.error(err.message);
@@ -44,10 +42,10 @@ router.post('/register', validInfo, async (req, res) => {
 //Login Route
 router.post('/login', validInfo, async (req, res) => {
   try {
-    // 1. Destructure the req.body
+    //Destructure the req.body
     const { email, password } = req.body;
 
-    // 2. Check if user doesn't exist (if not, throw error)
+    //Check if user doesn't exist (if not, throw error)
     const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [
       email
     ]);
@@ -55,7 +53,7 @@ router.post('/login', validInfo, async (req, res) => {
       return res.status(401).json('Password or Email is incorrect');
     }
 
-    // 3. Check if incoming password is the same as the database password
+    //Check if incoming password is the same as the database password
     const validPassword = await bcrypt.compare(
       password,
       user.rows[0].user_password
@@ -64,7 +62,7 @@ router.post('/login', validInfo, async (req, res) => {
       return res.status(401).json('Password or Email is incorrect');
     }
 
-    // 4. Give them the jwt token
+    //Give them the jwt token
     const token = jwtGenerator(user.rows[0].user_id);
     res.json({ token });
 
