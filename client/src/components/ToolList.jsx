73a5a60e3826ddfb375a8c768ae47cd95d437761
Michael Miller <millergm30@gmail.com>
2022-11-13 15:35:25 -0330

    just me being picky!

--- client/src/components/ToolList.jsx
+++ client/src/components/ToolList.jsx
@@ -41,49 +41,49 @@ const ToolList = () => {
   }, [newTools]);
 
   return (
-    <div className="flex flex-col bg-hero-pattern bg-no-repeat bg-center bg-cover bg-blend-luminosity bg-cyan-900" id="mainContainer">
+    <div className='flex flex-col bg-hero-pattern bg-no-repeat bg-center bg-cover bg-blend-luminosity bg-cyan-900' id='mainContainer'>
       <h3 className='text-center py-4 text-2xl font-extrabold'>Tool Chest</h3>
       {tools.length === 0 ? (
-        <table className="w-11/12 self-center md:w-3/4 lg:w-1/2 bg-white">
-          <thead className="bg-blue-300 border-b-2 border-stone-700 text-left">
+        <table className='w-11/12 self-center md:w-3/4 lg:w-1/2 bg-white'>
+          <thead className='bg-blue-300 border-b-2 border-stone-700 text-left'>
             <tr>
-              <th className="p-1">Type</th>
-              <th className="p-1">Brand</th>
-              <th className="p-1">Model</th>
-              <th className="p-1">Serial</th>
-              <th className="p-1 text-center">-</th>
+              <th className='p-1'>Type</th>
+              <th className='p-1'>Brand</th>
+              <th className='p-1'>Model</th>
+              <th className='p-1'>Serial</th>
+              <th className='p-1 text-center'>-</th>
             </tr>
           </thead>
-          <tbody className="text-center">
+          <tbody className='text-center'>
             <tr>
-              <td className="p-1" colSpan={4}>No tools added to inventory!</td>
+              <td className='p-1' colSpan={4}>No tools added to inventory!</td>
             </tr>
           </tbody>
         </table>
       ) : (
-        <table className="w-11/12 self-center md:w-3/4 lg:w-1/2 bg-white">
-          <thead className="bg-blue-300 border-b-2 border-stone-700 text-left">
+        <table className='w-11/12 self-center md:w-3/4 lg:w-1/2 bg-white'>
+          <thead className='bg-blue-300 border-b-2 border-stone-700 text-left'>
             <tr>
-              <th className="p-1">Type</th>
-              <th className="p-1">Brand</th>
-              <th className="p-1">Model</th>
-              <th className="p-1">Serial</th>
-              <th className="p-1 text-center">-</th>
+              <th className='p-1'>Type</th>
+              <th className='p-1'>Brand</th>
+              <th className='p-1'>Model</th>
+              <th className='p-1'>Serial</th>
+              <th className='p-1 text-center'>-</th>
             </tr>
           </thead>
-          <tbody className="text-left">
+          <tbody className='text-left'>
             {tools.map((tool) => (
               <tr
                 key={tool.tool_id}
-                className="border-b-2 border-stone-700 hover:bg-slate-200"
+                className='border-b-2 border-stone-700 hover:bg-slate-200'
               >
-                <td className="p-1">{tool.tool_type}</td>
-                <td className="p-1">{tool.tool_brand}</td>
-                <td className="p-1">{tool.tool_model}</td>
-                <td className="p-1">{tool.tool_serial}</td>
-                <td className="p-1 text-center">
+                <td className='p-1'>{tool.tool_type}</td>
+                <td className='p-1'>{tool.tool_brand}</td>
+                <td className='p-1'>{tool.tool_model}</td>
+                <td className='p-1'>{tool.tool_serial}</td>
+                <td className='p-1 text-center'>
                   <button
-                    className="text-black cursor-pointer m-0.5 text-xl hover:text-red-600"
+                    className='text-black cursor-pointer m-0.5 text-xl hover:text-red-600'
                     onClick={() => deleteTool(tool.tool_id)}
                   >
                     <TiDelete />
